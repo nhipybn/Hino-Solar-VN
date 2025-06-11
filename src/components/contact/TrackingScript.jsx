21e215d482fc2ef3fc2dc846773f4391e571b7b8
pham nhi <phamnhi@phams-MacBook-Air.local>
2025-06-11 10:49:09 +0700

    fix12

--- src/components/contact/TrackingScript.jsx
+++ src/components/contact/TrackingScript.jsx
@@ -1,18 +1,20 @@
-// BowNowForm.jsx
-const BowNowForm = () => {
-  return (
-     <iframe
-    allowtransparency="true"
-    frameborder="0"
-    height="800"
-    id="_bownow_form_contents"
-    scrolling="yes"
-    src="https://bownow.jp/contacts/72266/preview/fields?sid=sid_fcffccc68aa102d77cc0"
-    style="border: none; overflow: hidden;"
-    width="100%">
-  </iframe>
+import { useEffect } from 'react';
 
-  );
+const BowNowForm = () => {
+  useEffect(() => {
+    const scriptId = '_bownow_cs_sid_fcffccc68aa102d77cc0';
+
+    // Ngăn chặn thêm lại nếu đã có script
+    if (!document.getElementById(scriptId)) {
+      const script = document.createElement('script');
+      script.id = scriptId;
+      script.charset = 'utf-8';
+      script.src = 'https://contents.bownow.jp/forms/sid_fcffccc68aa102d77cc0/trace.js';
+      document.head.appendChild(script);
+    }
+  }, []);
+
+  return <div id="_bownow_form_container" className="min-h-[500px]" />;
 };
 
 export default BowNowForm;
